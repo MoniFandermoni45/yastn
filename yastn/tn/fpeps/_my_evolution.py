77ad78e297b99dda245b0c0168347b99ca53208e
MoniFandermoni45 <szajewskip7@gmail.com>
2025-11-01 18:44:19 +0100

    decompose_A_and_B done

--- yastn/tn/fpeps/_my_evolution.py
+++ yastn/tn/fpeps/_my_evolution.py
@@ -1,7 +1,9 @@
-from ... import tensordot, vdot, svd_with_truncation, YastnError, Tensor
-from ._peps import Peps2Layers
-from ._gates_auxiliary import Gate, gate_from_mpo
-from ..mps import MpsMpoOBC
+#from ... import tensordot, vdot, svd_with_truncation, YastnError, Tensor
+#from ._peps import Peps2Layers
+#from ._gates_auxiliary import Gate, gate_from_mpo
+#from ..mps import MpsMpoOBC
+
+#TODO: we need to correct the place where we have 'h' or 'lr' to and add parenthees
 
 import yastn.tn.fpeps as peps
 
@@ -12,10 +14,28 @@ import numpy as np
 
 # 1. Decompose correctly the tensors A and B
 def decompose_A_and_B(env, bond):
+    '''
+    Decompose the A and B w.r.t given bond.
+    returns: (Q0d, R0d, Q1d, R1d)
+    convention:
+        - for horizontal bond (left -> right):
+        Q0d: t l b rr s
+        R0d: rr r a
+
+        Q1d: t ll b r s
+        R1d: l ll a
+
+        - for vertical bond (top -> down)
+        Q0d: t l bb r s
+        R0d: bb b a
+
+        Q1d: tt l b r s
+        R1d: t tt a
+    '''
 
     psi = env.psi
 
-    if isinstance(psi, Peps2Layers):
+    if isinstance(psi, peps.Peps2Layers):
         psi = psi.ket  # to make it work with CtmEnv
 
     if bond is None: # if bond is not specified, take all bonds
@@ -25,78 +45,64 @@ def decompose_A_and_B(env, bond):
 
     for bond in bonds:
         dirn = psi.nn_bond_dirn(bond) # take the direction of the bond
+        print(dirn)
         s0, s1 = bond # if l_ordered else bond[::-1] # implement later
 
         tensor_A = psi[s0]
         tensor_B = psi[s1]
 
-        if dirn == 'h' or 'lr':  # Horizontal gate, "lr" ordered
 
-            #Raxis = 0 meaning specified axes go to the front
-            # we want to split the last axis before we do the QR
-            tensor_A = tensor_A.unfuse_legs(axes=-1) # t l b r s a <- what we assume
-            tensor_B = tensor_B.unfuse_legs(axes=-1) # t l b r s a
+        #Raxis = 0 meaning specified axes go to the front
+        # we want to split the last axis before we do the QR
+        tensor_A = tensor_A.unfuse_legs(axes=4) # t l b r s a <- what we assume
+        tensor_B = tensor_B.unfuse_legs(axes=4) # t l b r s a
+
+
+        if dirn == ('h' or 'lr'):  # Horizontal gate, "lr" ordered
+            print('doing horizontal bond')
 
             # perform QR:
-
             # we can immedietely specify the Q axis, we keep ancillas always at the end
-            Q0d, R0d = tmpA.qr(axes=((0, 1, 2, 4), (3, 5)), sQ=-1, Qaxis=3)  # t l b rr s @ rr r a
-            Q1d, R1d = tmpB.qr(axes=((0, 2, 3, 4), (1, 5)), sQ=1, Qaxis=0, Raxis=1)  # t b r s @ l ll a
+            Q0d, R0d = tensor_A.qr(axes=((0, 1, 2, 4), (3, 5)), sQ=-1, Qaxis=3)  # t l b rr s @ rr r a
+            Q1d, R1d = tensor_B.qr(axes=((0, 2, 3, 4), (1, 5)), sQ=1, Qaxis=1, Raxis=1)  # t ll b r s @ l ll a
 
-            #r0d = R0d.unfuse_legs(axes=2) # rr r s a
-            #r0d = r0d.swap_gate(axes=(1, 3)) # swap_gate r and a
-
-            #r1d = R1d.unfuse_legs(axes=1) # l s' a' ll
-            #r1d = r1d.swap_gate(axes=(2, 3)) # swap_gate a' and ll
-
-            #print('apply iter')
-            return 
-
-            # we include the section to introduce the EAT procedure
-
-            r0d, r1d, diff, num_of_iter = predisentangler_iter(r0d, r1d, D_total, max_iter, tol)
-            #print('new r0d:', r0d.get_shape())
-            #print('new r1d:', r1d.get_shape())
-
-            r0d = r0d.swap_gate(axes=(1, 3)) # swap_gate r and a
-            R0d = r0d.fuse_legs(axes=(0, 1, (2, 3)))
-
-            r1d = r1d.swap_gate(axes=(2, 3))
-            R1d = r1d.fuse_legs(axes=(0, (1, 2), 3))
-
-            tmpA = yastn.tensordot(Q0d, R0d, axes=(3, 0)) # t l b r sa
-
-            tmpB = yastn.tensordot(Q1d, R1d, axes=(0, 2)) # t b r l sa
-            tmpB = tmpB.transpose(axes=(0, 3, 1, 2, 4))   # t l b r sa
 
         else: # dirn == 'v':  # Vertical gate, "tb" ordered
 
-            Q0d, R0d = tmpA.qr(axes=((0, 1, 3), (2, 4)), sQ=-1)  # t l r bb @ bb b sa
+            print('doing vertical bond')
 
-            Q1d, R1d = tmpB.qr(axes=((1, 2, 3), (0, 4)), sQ=1, Qaxis=0, Raxis=-1)  # tt l b r @ t sa tt
+            Q0d, R0d = tensor_A.qr(axes=((0, 1, 3, 4), (2, 5)), sQ=-1, Qaxis=2)  # t l bb r s @ bb b a
+            Q1d, R1d = tensor_B.qr(axes=((1, 2, 3, 4), (0, 5)), sQ=1, Qaxis=0, Raxis=1)  # tt l b r s @ t tt a
 
-            r0d = R0d.unfuse_legs(axes=2) # bb b s a
-            r0d = r0d.swap_gate(axes=(1, 3)) # swap_gate b and a
+    return Q0d, R0d, Q1d, R1d
 
-            r1d = R1d.unfuse_legs(axes=1) # t s' a' tt
-            r1d = r1d.swap_gate(axes=(2, 3)) # swap_gate a' and tt
+def main():
+    '''tests'''
 
-            r0d, r1d, diff, num_of_iter = predisentangler_iter(r0d, r1d, D_total, max_iter, tol)
+    config = yastn.make_config(sym='dense')
 
-            r0d = r0d.swap_gate(axes=(1, 3)) # swap_gate r and a
-            R0d = r0d.fuse_legs(axes=(0, 1, (2, 3)))
+    # create two tensors
+    #legs = [
+    #    yastn.Leg(config, s=-1)
+    #]
 
-            r1d = r1d.swap_gate(axes=(2, 3))
-            R1d = r1d.fuse_legs(axes=(0, (1, 2), 3))
+    geometry = peps.CheckerboardLattice()
+    opt = yastn.operators.Spin12(sym='dense')
+    psi = peps.product_peps(geometry=geometry, vectors=opt.I())
+    env = peps.EnvNTU(psi, which='NN')
 
-            tmpA = yastn.tensordot(Q0d, R0d, axes=(3, 0)) # t l r b sa
-            tmpA = tmpA.transpose(axes=(0, 1, 3, 2, 4))   # t l b r sa
+    #print(env.psi[(0,0)].get_shape())
+    # try to decompose the last leg
+    #print(env.psi[(0,0)].unfuse_legs(axes=-1)) # the phsical has -1: outgoing, which make sense i guess, ancilla is 1 <- ingoing
 
-            tmpB = yastn.tensordot(Q1d, R1d, axes=(0, 2)) # l b r t sa
-            tmpB = tmpB.transpose(axes=(3, 0, 1, 2, 4))   # t l b r sa
+    # check the first bond
+    bond_lr = peps._geometry.Bond(peps._geometry.Site(0,0), peps._geometry.Site(1,0))
 
+    Q0d, R0d, Q1d, R1d = decompose_A_and_B(env, bond_lr)
+    print('Q0d:', Q0d.get_shape())
+    print('R0d:', R0d.get_shape())
+    print('Q1d:', Q1d.get_shape())
+    print('R1d:', R1d.get_shape())
 
-        psi[s0] = tmpA
-        psi[s1] = tmpB
-
-    return diff, num_of_iter
+if __name__ == '__main__':
+    main()
