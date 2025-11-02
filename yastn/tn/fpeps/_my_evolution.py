754fc925fc7cd85fd3256ba6fe994447cc19fc07
MoniFandermoni45 <szajewskip7@gmail.com>
2025-11-02 16:03:35 +0100

    my_predisentangler_iter done

--- yastn/tn/fpeps/_my_evolution.py
+++ yastn/tn/fpeps/_my_evolution.py
@@ -127,6 +127,16 @@ def my_evolution_step(env: peps.EnvNTU, gates, opts_svd, method='mpo', fix_metri
             # print('R1d:', R1d.get_shape())
             fgf = env.bond_metric(Q0=Q0d, Q1=Q1d, s0=s0, s1=s1, dirn=dirn)
 
+            # the metric for lr: [rr' ll'] [rr ll]
+            # the metric for tb: [bb' tt'] [bb tt]
+
+            # 3. Apply the metric to R0dR1d to get R0dR1d_tilde
+
+            # 4. Put this to modified Yintai's procedure -> we get (pre)disentangler
+
+            # 5. One way to continue is to take simply the svd of g applied to RR
+            # 6. Other way is to apply it nevertheless, in such way update the tensors and then put this to standard NTU procedure.
+
             # print(fgf)
 
             #diff, num_of_iter = peps.apply_predisentangler(env, bond, D_total=D_total, max_iter=max_iter) # this does not perform the truncation # I guess
@@ -136,6 +146,115 @@ def my_evolution_step(env: peps.EnvNTU, gates, opts_svd, method='mpo', fix_metri
             #infos.append(info)
         
     return infos
+
+
+def my_apply_predisentangler(r0d: yastn.Tensor, r1d: yastn.Tensor, D_total, max_iter=400, tol=1e-7):
+
+
+    r0d, r1d, diff, num_of_iter = my_predisentangler_iter(r0d, r1d, D_total, max_iter, tol)
+    # we can update the tensors
+
+    # We need to perform this back contraction:
+
+    r0d = r0d.swap_gate(axes=(1, 3)) # swap_gate r and a
+    R0d = r0d.fuse_legs(axes=(0, 1, (2, 3)))
+
+    r1d = r1d.swap_gate(axes=(2, 3))
+    R1d = r1d.fuse_legs(axes=(0, (1, 2), 3))
+
+    tmpA = yastn.tensordot(Q0d, R0d, axes=(3, 0)) # t l r b sa
+    tmpA = tmpA.transpose(axes=(0, 1, 3, 2, 4))   # t l b r sa
+
+    tmpB = yastn.tensordot(Q1d, R1d, axes=(0, 2)) # l b r t sa
+    tmpB = tmpB.transpose(axes=(3, 0, 1, 2, 4))   # t l b r sa
+
+
+    # we may set it
+    # psi[s0] = tmpA
+    # psi[s1] = tmpB
+
+    return diff, num_of_iter
+
+def my_predisentangler_iter(r0d:yastn.Tensor, r1d:yastn.Tensor, D_total, max_iter, tol=1e-7):
+    '''
+    returns: (
+    r0d: "left" reduced tensor,
+    r1d: "right" reduced tensor,
+    diff: difference in the convergence,
+    ii: number of iterations
+    )
+    '''
+
+    ii = 0
+    diff = 32767
+
+    #r0dr1d = yastn.tensordot(r0d, r1d, axes=(1, 0)) # contr. r and l
+    r0dr1d = yastn.tensordot(r0d, r1d, axes=(1,0)) # rr a ll a' (My version)
+    #r0dr1d = yastn.transpose(r0dr1d, axes=(0, 2, 1, 3)) # rr ll a a'
+
+    for ii in range(max_iter):
+
+        #u, s, v = yastn.svd_with_truncation(r0dr1d, axes=((0, 1, 2), (3, 4, 5)), sU=r0d.s[1], D_total=D_total)
+        u, s, v = yastn.svd_with_truncation(r0dr1d, axes=((0, 1), (2, 3)), sU=r0d.s[1], D_total=D_total) # U: rr a r   V^dag: l ll a'
+        r0dr1d_new = s.broadcast(u, axes=2)
+
+        # we obtain the truncated middle
+        r0dr1d_new = yastn.tensordot(r0dr1d_new, v, axes=(2, 0)) # rr a ll a' (My version)
+
+        g = build_predisentangler_g(r0dr1d, r0dr1d_new.conj()) # a a* a' a'* <- Yintai and my version
+
+        # xx s a s' a' yy <- Yintai version
+        # my version rr a ll a'
+        #r0dr1d_new = yastn.tensordot(r0dr1d, g, axes=((2, 4), (1, 3))) # xx s s' yy a a' Yintai version
+        r0dr1d_new = yastn.tensordot(r0dr1d, g, axes=((1, 3), (1, 3))) # rr ll a a' my version
+
+        # Divide onto left and right part (but first transpose after application of the disentangler):
+        #r0dr1d_new = r0dr1d_new.transpose(axes=(0, 1, 4, 2, 5, 3)) # xx s a s' a' yy Yintai version
+        r0dr1d_new = r0dr1d_new.transpose(axes=(0, 2, 3, 1)) # rr a a' ll 
+
+        # Normalization:
+        #r0dr1d_new = r0dr1d_new / (r0dr1d_new.fuse_legs(axes=((0, 1, 2), (3, 4, 5))).norm(p="fro"))
+        r0dr1d_new = r0dr1d_new / (r0dr1d_new.fuse_legs(axes=((0, 1), (2, 3))).norm(p="fro"))
+
+
+        # Calculate the error:
+            # For Yintai: r0dr1d: rr s a s' a' ll
+            # For me    : r0dr1d: rr a a' ll
+            #diff = (r0dr1d.fuse_legs(axes=((0, 1, 2), (3, 4, 5))) - r0dr1d_new.fuse_legs(axes=((0, 1, 2), (3, 4, 5)))).norm(p="fro") / r0dr1d.fuse_legs(axes=((0, 1, 2), (3, 4, 5))).norm(p="fro")
+        diff = (r0dr1d.transpose(axes=(0,1,3,2)).fuse_legs(axes=((0, 1), (2, 3))) - r0dr1d_new.fuse_legs(axes=((0, 1), (2, 3)))).norm(p="fro") / r0dr1d.fuse_legs(axes=((0, 1, 2), (3, 4, 5))).norm(p="fro")
+
+        r0dr1d = r0dr1d_new
+        r0dr1d = r0dr1d.transpose(axes=(0,1,3,2)) # rr a ll a'
+        # we may probably transpose back here to make sure we match the pattern rr a ll a'
+
+        if diff < tol:
+            # if the difference is sufficiently small
+
+
+            # Yintai version: r0dr1d: xx s a s' a' yy
+            # My version    : r0dr1d: rr a ll a'
+            #u, s, v = yastn.svd_with_truncation(r0dr1d, axes=((0, 1, 2), (3, 4, 5)), sU=r0d.s[1], D_total=r0d.get_shape(axes=1))
+            u, s, v = yastn.svd_with_truncation(r0dr1d, axes=((0,1), (2,3)), sU=r0d.s[1], UAxis = 1, D_total=r0d.get_shape(axes=1)) # u: r rr a, v: l ll a'
+
+            # Redestribute the singular values
+            s = s.sqrt()
+            #r0d = s.broadcast(u, axes=3).transpose(axes=(0, 3, 1, 2))
+            r0d = s.broadcast(u, axes=1) # rr r a
+            r1d = s.broadcast(v, axes=0) # l ll a'
+            break
+
+    return r0d, r1d, diff, ii
+
+def build_predisentangler_g(r0dr1d:yastn.Tensor, r0dr1d_conj:yastn.Tensor):
+    # r0dr1d xx s a s' a' yy <- Yintai version (xx==rr, yy==ll, probably)
+    # rodr1d rr a ll a' <- my version
+
+    #Eg = yastn.tensordot(r0dr1d, r0dr1d_conj, axes=((0, 1, 3, 5), (0, 1, 3, 5))) # a a' a* a'*  Yintai
+    Eg = yastn.tensordot(r0dr1d, r0dr1d_conj, axes=((0,2), (0, 2))) # a a' a* a'*
+    u, s, v = Eg.svd(axes=((0, 1), (2, 3)))
+    Eg = yastn.tensordot(v.conj(), u.conj(), axes=(0, 2)) # a* a'* a a' (&) # to be verified!
+    Eg = Eg.transpose(axes=(0, 2, 1, 3))
+    return Eg
 
 
 
